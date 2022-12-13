b0a7a876705792f949d8c715374bf21e649d2875
Marvin Korir <marvinkorir.mk@gmail.com>
2022-12-13 17:15:41 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+variable "cidr_blocks" {
+  description = "cidr blocks for vpc & subnets"
+   type = list(object({
+    cidr_block = string
+    name = string
+   }))
+}
+
+
+variable "environment"{
+  description = "deployment environment"
+  default = "production"
+}
+ variable "avail_zone"{
+
+ }
+
+resource "aws_vpc" "development-vpc"{
+    cidr_block = var.cidr_blocks[0].cidr_block
+    tags = {
+      Name: var.cidr_blocks[0].name
+    }
+}
+
+resource "aws_subnet" "dev-subnet-1"{
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.cidr_blocks[0].cidr_block 
+    availability_zone = var.avail_zone
+    tags = {
+      Name: var.cidr_blocks[1].name
+    }
+  }
+
+data "aws_vpc" "existing_vpc"{
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2"{
+  vpc_id = data.aws_vpc.existing_vpc.id
+  cidr_block = "172.31.96.0/20"
+  availability_zone = "us-east-1c"
+} 
+
+output "dev-vpc-id" {
+  value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet-id" {
+  value = aws_subnet.dev-subnet-1.id
+}
