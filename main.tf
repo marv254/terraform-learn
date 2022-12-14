3f982fc923707a5d0005acde7368c1eb35ebd00f
Marvin Korir <marvinkorir.mk@gmail.com>
2022-12-14 16:48:08 +0300

    added ec2 instance keypairs

--- main.tf
+++ main.tf
@@ -8,6 +8,9 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip{}
+variable instance_type{}
+variable key_name{}
+variable public_key_location{}
 
 resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
@@ -73,3 +76,47 @@ resource "aws_subnet" "myapp-subnet-1"{
     Name = "${var.env_prefix}-sg"
   }
 }
+
+data "aws_ami" "latest-amazon-linux-image"{
+  most_recent = true
+  owners = ["amazon"]
+  filter{
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+output aws_ami_id{
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_key_pair" "myapp-key" {
+  key_name           = "myapp-server-key"
+  public_key = "${file(var.public_key_location)}"
+
+
+}
+
+ resource "aws_instance" "myapp-server"{
+ ami = data.aws_ami.latest-amazon-linux-image.id
+ instance_type = var.instance_type
+
+ subnet_id = aws_subnet.myapp-subnet-1.id
+ vpc_security_group_ids = [aws_default_security_group.myapp-default-sg.id]
+ availability_zone=var.avail_zone
+ associate_public_ip_address = true
+ key_name = aws_key_pair.myapp-key.key_name
+ tags = {
+  Name = "${var.env_prefix}-myapp-server"
+ }
+ }
+
+ output ec2-ip {
+   value       = aws_instance.myapp-server.public_ip
+ }
+ 
